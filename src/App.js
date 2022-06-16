8199cd9785a8aeda9efd9164bb93c36a83263f78
AfsanaAbsar <afsanaabsarchy@gmail.com>
2022-06-16 22:57:46 +0600

    header created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './pages/Footer/Footer';
+import Home from './pages/Home/Home';
+import Header from './pages/Header/Header';
+import About from './pages/About/About';
+import Products from './pages/Products/Products';
+import Orders from './pages/Orders/Orders';
+import LogIn from './pages/User/LogIn/LogIn';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1>WOODEN</h1>
-    </div>
+    <>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/login" element={<LogIn />} />
+      </Routes>
+      <Footer></Footer>
+    </>
   );
 }
 
---
+++ src/pages/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" >
+                <Container>
+                    <Navbar.Brand href="#home">WOODEN</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Link to="/home">HOME</Link>
+                            <Link to="/about">ABOUT</Link>
+                            <Link to="/products">PRODUCTS</Link>
+                            <Link to="/orders">ORDERS</Link>
+                            <Link to="/login">LOG IN</Link>
+
+                        </Nav>
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
